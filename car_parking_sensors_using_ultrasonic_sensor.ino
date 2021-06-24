ff9cac0549b943e0265b3baaf5ba7e78556e4527
muralipinna <84914836+muralipinna@users.noreply.github.com>
2021-06-24 22:32:08 +0530

    Add files via upload

---
+++ car_parking_sensors_using_ultrasonic_sensor.ino
@@ -0,0 +1,37 @@
+const int trig =3;
+const int echo =4;
+const int buzzer =5;
+void setup() {
+pinMode(trig,INPUT);
+pinMode(echo,OUTPUT);
+Serial.begin(9600);
+pinMode(buzzer,OUTPUT);
+}
+
+void loop() {
+digitalWrite(trig,LOW);
+delayMicroseconds(2);
+digitalWrite(trig,HIGH);
+delayMicroseconds(10);
+digitalWrite(trig,LOW);
+
+int v = pulseIn(echo,HIGH);
+float dis = (0.0001)*343*(v/2);
+Serial.print("distance :");
+Serial.println(dis);
+if(dis< 5.0){
+  tone(buzzer,1200);
+  delay(500);
+}
+else if(dis<12.0){
+  
+  tone(buzzer,1000);
+  delay(500);
+}
+else
+{
+  noTone(buzzer);
+delay(500);
+  }
+
+}
